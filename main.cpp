f53fbe108cdc93c9ed9afb4d8755ad12346ab238
Pupler <misha.reak45@gmail.com>
2025-11-27 18:30:55 +0100

    Main loop and request reading

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 int main() {
     // Create socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        std::cerr << "Socket failed" << std::endl;
+        return 1;
+    }
     
     // Configure address
     sockaddr_in address{};
@@ -14,26 +18,47 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
     
-    // Bind socket to port
-    bind(server_fd, (sockaddr*)&address, sizeof(address));
+    // Bind socket
+    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        return 1;
+    }
     
     // Start listening
-    listen(server_fd, 5);
+    if (listen(server_fd, 5) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        return 1;
+    }
+    
     std::cout << "Server listening on port 8080...\n";
     
-    // Accept single connection
-    int client_fd = accept(server_fd, nullptr, nullptr);
+    // MAIN LOOP - handle multiple clients
+    while(true) {
+        // Accept connection
+        int client_fd = accept(server_fd, nullptr, nullptr);
+        if (client_fd < 0) {
+            std::cerr << "Accept failed" << std::endl;
+            continue;
+        }
+        
+        // Read HTTP request
+        char buffer[1024] = {0};
+        int bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+        if (bytes_read > 0) {
+            std::cout << "Received request:\n" << buffer << std::endl;
+        }
+        
+        // Send HTTP response
+        const char* response = 
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "\r\n"
+            "Hello from C++ server!";
+        send(client_fd, response, strlen(response), 0);
+        
+        close(client_fd);
+    }
     
-    // Send basic HTTP response
-    const char* response = 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        "\r\n"
-        "Hello from C++ server!";
-    send(client_fd, response, strlen(response), 0);
-    
-    // Cleanup
-    close(client_fd);
     close(server_fd);
     return 0;
 }
